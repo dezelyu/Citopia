c740c7d3921ef70695e3f26e15c8f969ced54dcd
dezelyu <dezelyu@seas.upenn.edu>
2024-11-08 00:26:51 -0500

    Update project structure

---
+++ Sources/Citopia/Citopia.swift
@@ -0,0 +1,7 @@
+
+// import the Metal graphics API
+import MetalKit
+
+// define the class for performing the simulation
+class Citopia {
+}
---
+++ Sources/Renderer/Renderer.swift
@@ -0,0 +1,13 @@
+
+// import the custom game engine
+import ApplicationKit
+import GraphicsKit
+import AssetKit
+import NodeKit
+import MeshKit
+import MotionKit
+import CameraKit
+
+// define the class for rendering the simulation
+class Renderer {
+}
